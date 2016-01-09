b55aad72ca168cf95b04cc0fba6aed583ce61d31
itsource <ggz@itsource.cn>
2016-01-09 15:54:24 +0800

    添加一个gii的控制器

---
+++ admin.shop.com/Application/Admin/Controller/GiiController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/1/9
+ * Time: 15:52
+ */
+
+namespace Admin\Controller;
+
+
+use Think\Controller;
+
+class GiiController extends Controller
+{
+
+}

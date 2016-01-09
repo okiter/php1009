9ce15721036326b37db4409abaf6fee9ee91bd8f
itsource <ggz@itsource.cn>
2016-01-09 15:29:34 +0800

    完成了基础代码

---
+++ admin.shop.com/Application/Admin/Controller/BrandController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Admin\Controller;
+
+
+use Think\Controller;
+
+class BrandController extends BaseController
+{
+    protected $meta_title = '商品品牌';
+
+}
---
+++ admin.shop.com/Application/Admin/Controller/SupplierController.class.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Admin\Controller;
+
+
+use Think\Controller;
+
+class SupplierController extends BaseController
+{
+    protected $meta_title = '供应商';
+    //因为该方法在父类构造函数中被调用,该方法的名字必须是: _initialize
+
+}
